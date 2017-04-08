e06932359997803e24bf99587cf385b0a6699063
Johannes <johannes@jitesoft.com>
2017-04-08 20:58:50 +0200

    Update run.sh

--- run.sh
+++ run.sh
@@ -6,17 +6,20 @@
 
 cd /nunit
 nuget install NUnit.Console
+# Fetch the name of the console runner (using -d with a pattern cause we want to find any version of it).
 NUNIT=$(ls -d *NUnit.ConsoleRunner.*/)
 NUNIT_PATH="/nunit/${NUNIT}tools/nunit3-console.exe"
 # Move the source so that restore don't add a bunch of packages to the folder.
 cp -R /app /usr/src/app/source
 cd /usr/src/app/source
 # Build all solutions in the SOLUTIONS env variable.
+COUNT=0
 for SLN in ${SOLUTIONS//,/ }
 do
-    mkdir /usr/src/app/build/${SLN}
+    mkdir /usr/src/app/build/${COUNT}/
     nuget restore -NonInteractive $SLN
-    xbuild /property:Configuration=Release /property:OutDir=/usr/src/app/build/${SLN}/ $SLN
+    xbuild /property:Configuration=Release /property:OutDir=/usr/src/app/build/${COUNT}/ $SLN
+    let COUNT=COUNT+1
 done
 cd /usr/src/app/source
 
